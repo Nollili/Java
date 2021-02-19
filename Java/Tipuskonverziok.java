e7b12dc8f05be802445f8bdde8dc4d3dd8c7e7c1
Nollili <66601688+Nollili@users.noreply.github.com>
2021-02-19 16:43:53 +0100

    Update Tipuskonverziok.java

--- Java/Tipuskonverziok.java
+++ Java/Tipuskonverziok.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tipuskonverziok;
 
-/**
- *
- * @author Seres Sándor
- */
 public class Tipuskonverziok {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //Automatikus típuskonverzió
         char c = 'c';
