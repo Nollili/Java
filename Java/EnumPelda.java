f9fa74af1276e356ccdea7228ecf9c8a7c22d895
Nollili <66601688+Nollili@users.noreply.github.com>
2021-02-19 16:43:20 +0100

    Update EnumPelda.java

--- Java/EnumPelda.java
+++ Java/EnumPelda.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package enumpelda;
 
-/**
- *
- * @author Seres Sándor
- */
 public class EnumPelda {
 
-    /**
-     * @param args the command line arguments
-     */
     public enum Allat{kutya, macska, farkas, tigris}
     
     public static void main(String[] args) {
